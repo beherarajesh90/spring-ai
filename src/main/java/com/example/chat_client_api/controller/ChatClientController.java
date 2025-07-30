ffcfa56ff894a145673e08e41af94632143cf438
Rajesh <beherarajesh90@gmail.com>
2025-07-30 19:38:26 +0530

    return an entity as response

--- src/main/java/com/example/chat_client_api/controller/ChatClientController.java
+++ src/main/java/com/example/chat_client_api/controller/ChatClientController.java
@@ -1,5 +1,6 @@
 package com.example.chat_client_api.controller;
 
+import com.example.chat_client_api.entiy.ActorFilms;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -20,4 +21,13 @@ public class ChatClientController {
                 .call()
                 .content();
     }
+
+    @GetMapping("/ai-entity")
+    public ActorFilms generateResponse(){
+        return this.chatClient.prompt()
+                .user("Return top 5 Salman Khan movies.")
+                .call()
+                .entity(ActorFilms.class);
+    }
+
 }
---
+++ src/main/java/com/example/chat_client_api/entiy/ActorFilms.java
@@ -0,0 +1,5 @@
+package com.example.chat_client_api.entiy;
+
+import java.util.List;
+
+public record ActorFilms(String actor, List<String> movies) {}
